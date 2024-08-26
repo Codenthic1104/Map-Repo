eb42b2a39b8991b3cac98618a3772af611b22506
arhammunir1104 <munirarham70@gmail.com>
2024-08-26 09:38:20 -0700

    adding more components

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -56,7 +56,7 @@ function Hero() {
 
     return (
         <div className='relative'>
-            <div className='absolute inset-0 blur-xl h-[580px]' style={{ background: "linear-gradient(143.6deg, rgba(192, 132, 252, 0) 20.79%, rgba(232, 121, 249, 0.26) 40.92%, rgba(204, 171, 238, 0) 70.35%)" }}></div>
+            <div className='absolute inset-0 blur-xl h-[580px]' style={{ background: "linear-gradient(143.6deg, rgba(99, 102, 241, 0.1) 20.79%, rgba(99, 102, 241, 0.36) 40.92%, rgba(99, 102, 241, 0.1) 70.35%)" }}></div>
             <div className='relative'>
                 <header>
                     <div className={`md:hidden ${state ? "mx-2 pb-5" : "hidden"}`}>
